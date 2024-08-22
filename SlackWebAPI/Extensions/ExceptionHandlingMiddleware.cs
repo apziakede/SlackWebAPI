471c58514ef741afb8ad6743c67f750b47fcdf87
apziakede <168096979+apziakede@users.noreply.github.com>
2024-08-22 15:55:36 +0100

    Update ExceptionHandlingMiddleware.cs

--- SlackWebAPI/Extensions/ExceptionHandlingMiddleware.cs
+++ SlackWebAPI/Extensions/ExceptionHandlingMiddleware.cs
@@ -15,18 +15,41 @@ namespace SlackWebAPI.Extensions
 
         public async Task InvokeAsync(HttpContext context)
         {
-            try
-            {
-                await _next(context);
-            }
-            catch (Exception ex)
-            {
-                if (!string.IsNullOrEmpty(ex.Message))
-                {
-                    await _slackService.SendMessageAsync($"{ex.Source}: {ex} \n\n[Error {DateTime.UtcNow.AddHours(1):MM/dd/yy HH:mm:ss}] {context.Response.StatusCode}: {ex.Message}");
-                }
-                await Task.CompletedTask;
-            }
+             // Create a memory stream to capture the response body
+  var originalBodyStream = context.Response.Body;
+  using var newBodyStream = new MemoryStream();
+  context.Response.Body = newBodyStream;
+  try
+  { 
+      await _next(context);
+      //if(context.Response.StatusCode==StatusCodes.Status401Unauthorized)
+      //{
+      //    await _slackService.SendMessageAsync($"[Warning {DateTime.UtcNow.AddHours(1):MM/dd/yy HH:mm:ss}] {context.Response.StatusCode}: Unauthorized request made to {context.Request.Path}");
+      //}
+
+      // Copy the contents of the new body stream to a string
+      newBodyStream.Seek(0, SeekOrigin.Begin);
+      var responseBody = await new StreamReader(newBodyStream).ReadToEndAsync();
+
+      // Log the response body and status code
+      if (context.Response.StatusCode >= 400)
+      {
+          await _slackService.SendMessageAsync($"Response Status Code: {context.Response.StatusCode}, Response Body: {responseBody}");
+      }
+
+      // Reset the stream position to the beginning and copy it back to the original stream
+      newBodyStream.Seek(0, SeekOrigin.Begin);
+      await newBodyStream.CopyToAsync(originalBodyStream);
+
+  }
+  catch (Exception ex)
+  {
+      if (!string.IsNullOrEmpty(ex.Message))
+      {
+          await _slackService.SendMessageAsync($"{ex.Source}: {ex} \n\n[Error {DateTime.UtcNow.AddHours(1):MM/dd/yy HH:mm:ss}] {context.Response.StatusCode}: {ex.Message}");
+      }
+      await Task.CompletedTask;
+  }
         }
     }
 }
